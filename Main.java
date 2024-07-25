7edf94858ea9fde97b750355b99bfc1544ececa9
Chocorot <79820753+Chocorot@users.noreply.github.com>
2024-07-25 14:37:25 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
